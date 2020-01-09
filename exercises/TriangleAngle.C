3ae4f4360dfe954287233167a73ae2726debdadd
Lauricenia <54823733+Lauricenia@users.noreply.github.com>
2020-01-09 13:50:52 +0900

    Update TriangleAngle.C

--- exercises/TriangleAngle.C
+++ exercises/TriangleAngle.C
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+//This program reads temperature in degrees and displays a message according to the temperature state 
+
+int main() {
+  int angle1,angle2,angle3, total;
+  //ask the user for the temperature 
+  printf("Enter the angles\n");
+  scanf("%d %d %d", &angle1, &angle2, &angle3);
+  total = angle1 + angle2 + angle3;
+ //if temperature 
+ if (total == 180)
+{
+
+  printf("The triangle is valid\n");
+}
+else
+{
+  printf("The triangle is not valid\n");
+}
+
+  return 0; 
+}
