7b7bcc772f5d9766a47eeb2a418610cd1b281c4c
Lauricenia <54823733+Lauricenia@users.noreply.github.com>
2020-01-10 08:10:10 +0900

    Update quadrant.c

--- exercises/quadrant.c
+++ exercises/quadrant.c
@@ -4,22 +4,28 @@
 int main() {
   int X, Y;
   //ask the user for the coordinate
-  printf("Enter the coordinate point.\n");
+  printf("Enter the XY coordinates.\n");
   scanf("%i %i",&X, &Y);
   
+  //first quadrant
   if ( X > 0 && Y > 0)
   {
   printf("The coordinate point (%i, %i) lies in the first quadrant\n", X, Y);
   }
-
+ //second quadrant
   if ( X < 0 && Y > 0)
-  printf("The coordinate point (%i, %i) lies in the second quadrant\n", X, Y);
-
+  {
+    printf("The coordinate point (%i, %i) lies in the second quadrant\n", X, Y);
+  }
+  //third quadrant
   if ( X < 0 && Y < 0)
-  printf("The coordinate point (%i, %i) lies in the third quadrant\n", X, Y);
-  
+  {
+    printf("The coordinate point (%i, %i) lies in the third quadrant\n", X, Y);
+  }
+  //fourth quadrant
   if ( X > 0 && Y < 0)
-  printf("The coordinate point (%i, %i) lies in the fourth quadrant\n", X, Y);
-
+  {
+    printf("The coordinate point (%i, %i) lies in the fourth quadrant\n", X, Y);
+  }
   return 0; 
 }
